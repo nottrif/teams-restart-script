f17e7e6e742f174bd514c49806012db99f589aee
Abishek Ayyappan <jettleetrif@gmail.com>
2023-06-20 16:00:46 +0530

    feat: add script to fetch PIDand kill process

--- teams.sh
+++ teams.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+process_name="teams-for-linux"
+parameter="--type=renderer"
+pid=$(ps aux | grep "$process_name" | awk -v param="$parameter" '$0 ~ param && !/grep/ {print $2}')
+kill -15 "$pid"
 
