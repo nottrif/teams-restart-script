5586382e9451e36ef4b013ea79adb9737eb94aef
Abishek Ayyappan <jettleetrif@gmail.com>
2023-06-27 11:24:13 +0530

    feat: add command to start application

--- teams.sh
+++ teams.sh
@@ -11,5 +11,5 @@ echo "teams application has been killed"
 
 path = /var/lib/flatpak/app/com.github.IsmaelMartinez.teams_for_linux/x86_64/stable/b6472a071582e475b5f91afa6ac7cfac1bfc81f89e80e1f2ff032007e0bc0882/files/teams-for-linux/teams-for-linux
 
+# this spawns a new child process
 sudo path
-#
