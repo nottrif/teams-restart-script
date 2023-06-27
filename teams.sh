0b0d2b294e17db13322d491afb07f097a26d2923
Abishek Ayyappan <jettleetrif@gmail.com>
2023-06-27 11:19:00 +0530

    feat: add path to teams-for-linux binary

--- teams.sh
+++ teams.sh
@@ -6,3 +6,5 @@ pid=$(ps aux | grep "$process_name" | awk -v param="$parameter" '$0 ~ param && !
 kill -15 "$pid"
 
 echo "teams application has been killed"
+
+path = /var/lib/flatpak/app/com.github.IsmaelMartinez.teams_for_linux/x86_64/stable/b6472a071582e475b5f91afa6ac7cfac1bfc81f89e80e1f2ff032007e0bc0882/files/teams-for-linux/teams-for-linux
