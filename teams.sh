cf4d02499976adaf3a5ebf0a0f8f20ebe1c4537b
Abishek Ayyappan <jettleetrif@gmail.com>
2023-06-30 13:59:04 +0530

    feat: add supporting echo messages

--- teams.sh
+++ teams.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "---RESTARTING TEAMS APPLICATION---"
+echo " "
 
 process_name="teams-for-linux"
 parameter="--type=renderer"
@@ -8,9 +9,16 @@ pid=$(ps aux | grep "$process_name" | awk -v param="$parameter" '$0 ~ param && !
 kill -15 "$pid"
 
 echo "teams application has been killed"
+echo " "
 
-path = "/var/lib/flatpak/exports/share/applications/com.microsoft.Teams.desktop"
+#Please change the path to your teams application path
+path="/var/lib/flatpak/exports/share/applications/com.github.IsmaelMartinez.teams_for_linux.desktop"
+
+echo "starting teams application"
+echo " "
 
 command=$(grep -oP '(?<=^Exec=).*' "$path")
 
 nohup $command > /dev/null 2>&1 &
+
+echo "teams application has been restarted"
