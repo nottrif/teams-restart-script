85c1d04186a99bb353c1cb6ae0d010215d3fc5da
Abishek Ayyappan <jettleetrif@gmail.com>
2023-06-30 13:34:43 +0530

    fix: update path to teams desktop entry

--- teams.sh
+++ teams.sh
@@ -9,7 +9,6 @@ kill -15 "$pid"
 
 echo "teams application has been killed"
 
-path = /var/lib/flatpak/app/com.github.IsmaelMartinez.teams_for_linux/x86_64/stable/b6472a071582e475b5f91afa6ac7cfac1bfc81f89e80e1f2ff032007e0bc0882/files/teams-for-linux/teams-for-linux
+path = /var/lib/flatpak/exports/share/applications/com.github.IsmaelMartinez.teams_for_linux.desktop
 
-# this spawns a new child process
-sudo path
+
